64200418358c5186e1ea21c8a2e16e1247e49519
Debbora Leip <leip@pik-potsdam.de>
2024-04-24 22:43:16 +0200

    fix lint in readLPJmLInputs

--- R/readLPJmLInputs.R
+++ R/readLPJmLInputs.R
@@ -18,45 +18,45 @@
 
 readLPJmLInputs <- function(subtype = "lakeshare") {
 
-   files <- c(lakeshare  = "glwd_lakes_and_rivers.bin")
-   file  <- toolSubtypeSelect(subtype, files)
+  files <- c(lakeshare  = "glwd_lakes_and_rivers.bin")
+  file  <- toolSubtypeSelect(subtype, files)
 
-   # Data settings
-   if (subtype %in% c("lakeshare")) {
+  # Data settings
+  if (subtype %in% c("lakeshare")) {
 
-      unitTrans       <- 0.01
-      ncells          <- 67420
-      wyears          <- 1
-      syear           <- 1
-      avgRange        <- NULL
-      filetype       <- "bin"
-      bands           <- 1
-      datatype        <- integer()
-      bytes           <- 1
-      monthly         <- FALSE
-   }
+    unitTrans       <- 0.01
+    ncells          <- 67420
+    wyears          <- 1
+    syear           <- 1
+    avgRange        <- NULL
+    filetype       <- "bin"
+    bands           <- 1
+    datatype        <- integer()
+    bytes           <- 1
+    monthly         <- FALSE
+  }
 
-   # Read in the data
-   x <- readLPJ(file_name       = file,
-                wyears          = wyears,
-                syear           = syear,
-                averaging_range = avgRange,
-                ncells          = ncells,
-                file_type       = filetype,
-                bands           = bands,
-                datatype        = datatype,
-                bytes           = bytes,
-                monthly         = monthly)
+  # Read in the data
+  x <- readLPJ(file_name       = file,
+               wyears          = wyears,
+               syear           = syear,
+               averaging_range = avgRange,
+               ncells          = ncells,
+               file_type       = filetype,
+               bands           = bands,
+               datatype        = datatype,
+               bytes           = bytes,
+               monthly         = monthly)
 
-   # Unit transformation
-   x <- x * unitTrans
+  # Unit transformation
+  x <- x * unitTrans
 
-   # Transform to magpie object and add dimension details
-   class(x) <- "array"
-   x        <- collapseNames(as.magpie(x, spatial = 1))
-   x        <- addLocation(x)
-   x        <- collapseDim(x, dim = "N")
-   x        <- clean_magpie(x)
+  # Transform to magpie object and add dimension details
+  class(x) <- "array"
+  x        <- collapseNames(as.magpie(x, spatial = 1))
+  x        <- addLocation(x)
+  x        <- collapseDim(x, dim = "N")
+  x        <- clean_magpie(x)
 
-   return(x)
+  return(x)
 }
