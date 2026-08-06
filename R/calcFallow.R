aecb6997193c4ba8e775120dffd31aa343fff4ab
FelicitasBeier <beier@pik-potsdam.de>
2026-08-06 16:37:09 +0200

    remove physical argument from calcFallow, remove selectyears argument from calcForestArea to get consistent forest area (including no primary forest growth) for all function calls,

--- R/calcFallow.R
+++ R/calcFallow.R
@@ -1,15 +1,11 @@
 #' @title calcFallow
-#' @description
-#' Returns fallow land of our default datasource.
-#' The estimates are scaled to match our Land classes in the Croparea function.
-#' Due to multiple cropping, harvested cropland area can be greater than non-fallow land area
-#' and even greater than physical cropland area.
-#' @param cellular TRUE for cellular outputs.
-#' @param physical    if TRUE it returns the physical area, with fallow of multicropping systems being
-#' scaled to match physical areas of the rotation, if FALSE the area harvested
-#' being fallow is returned
+#' @description Returns fallow land calculated based on physical cropland extent
+#'              and harvested area output of default croparea datasource.
+#'              The resultant areas are scaled to match the land croparea land
+#'              class of calcLanduseInitialisation.
+#' @param cellular TRUE for cellular outputs
 #' @return MAgPIE object containing fallow land in Mha
-#' @author David Hoetten, Felicitas Beier
+#' @author David Hoetten, Felicitas Beier, Benjamin Bodirsky
 #' @seealso
 #' \code{\link{readLandInG}}
 #' @examples
@@ -19,10 +15,10 @@
 #' @importFrom magclass dimSums mbind
 #' @importFrom madrat toolConditionalReplace
 #'
-calcFallow <- function(cellular = FALSE, physical = TRUE) {
+calcFallow <- function(cellular = FALSE) {
 
   fallow <- calcOutput("Croparea", cellular = cellular,
-                       physical = physical, fallow = TRUE,
+                       physical = TRUE, fallow = TRUE,
                        irrigation = FALSE, aggregate = FALSE)
   fallow <- fallow[, , "fallow"]
 
